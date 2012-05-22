c2c5600bc7e544a73dbbc097d5c089374d14d026
xandy <gitmail.xandy@xoxy.net>
2012-05-22 19:27:03 +0200

    refacotred & reformated code

--- rc.lua
+++ rc.lua
@@ -64,50 +64,52 @@ mypromptbox = {}
 mylayoutbox = {}
 mytaglist = {}
 mytaglist.buttons = awful.util.table.join(
-                    awful.button({ }, 1, awful.tag.viewonly),
-                    awful.button({ modkey }, 1, awful.client.movetotag),
-                    awful.button({ }, 3, awful.tag.viewtoggle),
-                    awful.button({ modkey }, 3, awful.client.toggletag),
-                    awful.button({ }, 4, awful.tag.viewnext),
-                    awful.button({ }, 5, awful.tag.viewprev)
+                      awful.button({ }, 1, awful.tag.viewonly),
+                      awful.button({ modkey }, 1, awful.client.movetotag),
+                      awful.button({ }, 3, awful.tag.viewtoggle),
+                      awful.button({ modkey }, 3, awful.client.toggletag),
+                      awful.button({ }, 4, awful.tag.viewnext),
+                      awful.button({ }, 5, awful.tag.viewprev)
                     )
 mytasklist = {}
 mytasklist.buttons = awful.util.table.join(
-                     awful.button({ }, 1, function (c)
-                                              if not c:isvisible() then
-                                                  awful.tag.viewonly(c:tags()[1])
-                                              end
-                                              client.focus = c
-                                              c:raise()
-                                          end),
-                     awful.button({ }, 3, function ()
-                                              if instance then
-                                                  instance:hide()
-                                                  instance = nil
-                                              else
-                                                  instance = awful.menu.clients({ width=250 })
-                                              end
-                                          end),
-                     awful.button({ }, 4, function ()
-                                              awful.client.focus.byidx(1)
-                                              if client.focus then client.focus:raise() end
-                                          end),
-                     awful.button({ }, 5, function ()
-                                              awful.client.focus.byidx(-1)
-                                              if client.focus then client.focus:raise() end
-                                          end))
+                        awful.button({ }, 1, function (c)
+                            if not c:isvisible() then
+                                awful.tag.viewonly(c:tags()[1])
+                            end
+                            client.focus = c
+                            c:raise()
+                        end),
+                        awful.button({ }, 3, function ()
+                            if instance then
+                                instance:hide()
+                                instance = nil
+                            else
+                                instance = awful.menu.clients({ width=250 })
+                            end
+                        end),
+                        awful.button({ }, 4, function ()
+                            awful.client.focus.byidx(1)
+                            if client.focus then client.focus:raise() end
+                        end),
+                        awful.button({ }, 5, function ()
+                            awful.client.focus.byidx(-1)
+                            if client.focus then client.focus:raise() end
+                        end)
+                    )
 
 for s = 1, screen.count() do
-    -- Create a promptbox for each screen
     mypromptbox[s] = awful.widget.prompt({ layout = awful.widget.layout.horizontal.leftright })
-    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
+
+    --layoutbox
     mylayoutbox[s] = awful.widget.layoutbox(s)
     mylayoutbox[s]:buttons(awful.util.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc(layouts, 1)  end),
-                           awful.button({ }, 3, function () awful.layout.inc(layouts, -1) end),
-                           awful.button({ }, 4, function () awful.layout.inc(layouts, 1)  end),
-                           awful.button({ }, 5, function () awful.layout.inc(layouts, -1) end)))
+                                awful.button({ }, 1, function () awful.layout.inc(layouts, 1)  end),
+                                awful.button({ }, 3, function () awful.layout.inc(layouts, -1) end),
+                                awful.button({ }, 4, function () awful.layout.inc(layouts, 1)  end),
+                                awful.button({ }, 5, function () awful.layout.inc(layouts, -1) end)
+                            )
+                          )
     -- Create a taglist widget
     mytaglist[s] = awful.widget.taglist(s, awful.widget.taglist.label.all, mytaglist.buttons)
 
@@ -140,7 +142,6 @@ end
 
 -- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 7, awful.tag.viewnext),
     awful.button({ }, 6, awful.tag.viewprev)
 ))
@@ -150,8 +151,6 @@ root.buttons(awful.util.table.join(
 globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext),
-    --awful.key({                   }, 4, awful.tag.viewprev),
-    --awful.key({                   }, 7, awful.tag.viewnext),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
 
     awful.key({ modkey,           }, "j",
@@ -189,9 +188,9 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return",function () awful.util.spawn(terminal) end),
-    awful.key({ modkey, "Control" }, "r", awesome.restart),
-    --awful.key({ modkey, "Shift"   }, "q", awesome.quit),
-
+    awful.key({ modkey,           }, "l",     function () awful.util.spawn("slock") end),
+    awful.key({ modkey,           }, "Print", function () awful.util.spawn("scrot -s") end),
+    awful.key({ modkey, "Control" }, "r",                 awesome.restart),
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
     awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
@@ -214,7 +213,6 @@ globalkeys = awful.util.table.join(
     --old fluxbox bindings
     awful.key({ "Mod1"            }, "F1",    function () awful.util.spawn(terminal) end),
     awful.key({ "Mod1"            }, "F2",    function () mypromptbox[mouse.screen]:run() end)
-    --file manager here ;)
 )
 
 clientkeys = awful.util.table.join(
